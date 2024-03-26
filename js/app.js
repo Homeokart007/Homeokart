049fa86ed97542dcb363d7af527fd48abc097dbc
Smit Bhamwala <smitbhamwala@gmail.com>
2024-03-26 22:31:47 +0530

    Updated app.js to index.js

--- js/app.js
+++ js/app.js
@@ -1,20 +1,19 @@
-// const express = require('express');
-// const bodyParser = require('body-parser')
-// const ejs = require('ejs');
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const ejs = require("ejs");
 
 // const app = express();
 
-// app.set('view engine','ejs')
+// app.set("view engine", "ejs");
 
-// app.use(bodyParser.urlencoded({ extended:true }))
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(express.static("public"))
+// app.use(express.static("public"));
 
-// app.get("/",function(req,res){
+// app.get("/", function (req, res) {
+// 	// res.render('homepage');
+// });
 
-//     // res.render('homepage');
-// })
-
-// app.listen(3000,(req,res) => {
-//     console.log("Server started on port 3000..!!!")
-// })
+// app.listen(3000, (req, res) => {
+// 	console.log("Server started on port 3000..!!!");
+// });
